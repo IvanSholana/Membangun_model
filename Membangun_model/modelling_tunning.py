7b241830ca31f10a8094d8ebbccce72a70513001
Ivan Sholana <ivansholana@gmail.com>
2025-06-21 20:18:13 +0700

    Refactor MLflow tracking URI setup and remove dotenv dependency

--- Membangun_model/modelling_tunning.py
+++ Membangun_model/modelling_tunning.py
@@ -2,7 +2,6 @@ import os
 import sys
 import time
 import pandas as pd
-from dotenv import load_dotenv
 
 from xgboost import XGBClassifier
 from sklearn.model_selection import GridSearchCV, train_test_split
@@ -13,8 +12,7 @@ import mlflow.xgboost
 import dagshub
 
 # ============== LOAD ENV ==============
-load_dotenv()  # From .env file
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+mlflow.set_tracking_uri("https://dagshub.com/IvanSholana/Membangun_model.mlflow")
 
 # ============== DAGSHUB INIT ==============
 dagshub.init(
